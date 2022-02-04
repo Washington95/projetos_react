8d784d89960a9b5f5a367f95c626f860ef88091f
Washington95 <wst-washington@hotmail.com>
2022-02-04 20:17:33 -0300

    conceito de children e fragment

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,11 +1,15 @@
 import { Header } from "./components/Header";
+import { Photo } from "./components/photo";
 
 const App = () =>{
   return(
     <div>
       <Header title="Novo Texto"/>
       Olá mundo!
+
+      <Photo url="http://www.google.com.br/google.jpg" legend="Google"/>
     </div>
+    
   )
 }
 export default App;
