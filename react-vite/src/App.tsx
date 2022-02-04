0a0652b6cd67c7491ae98c778e67e500810d3cc9
Washington95 <wst-washington@hotmail.com>
2022-02-04 18:55:08 -0300

    expressões no jsx

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+// const App = () =>{
+//   let n1: number = 10;
+//   let n2: number = 18;
 
-const App = () =>{
+//   function somar(n1:number, n2:number): number{
+//     return n1+ n2
+
+//   }
+
+//   return(
+//     <div>Soma : {somar(n1, n2)}</div>
+//   )
+
+// }
+// const App = () =>{
+//   let name: string = 'Washington';
+//   let lastname: string = 'Luiz'
+
+//   return(
+//     <div>{`${name.toLowerCase()} ${lastname.toLocaleUpperCase()}`}</div>
+//   )
+// }
+
+const  App = () =>{
+  let link : string = 'https://b7web.com.br'
+
   return(
-    <a href="https://b7web.com.br">Clique aqui</a>
+    <div>
+      Acesse o site <br />
+      <a href={link}>Clique aqui</a>
+    </div>
   )
 }
-
 export default App;
