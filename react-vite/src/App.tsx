c67962ef32d2954ec1fe4e4a8043393e5c2e0e6c
Washington95 <wst-washington@hotmail.com>
2022-02-05 20:17:59 -0300

    criando eventos

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,13 +1,13 @@
-import { Header } from "./components/Header";
-import { Photo } from "./components/photo";
-
 const App = () =>{
+
+  const clicou = () =>{
+    alert('Clicou')
+  }
+  
   return(
     <div>
-      <Header title="Novo Texto"/>
-      Olá mundo!
+     <button onClick={clicou}>Clique aqui</button>
 
-      <Photo url="http://www.google.com.br/google.jpg" legend="Google"/>
     </div>
     
   )
