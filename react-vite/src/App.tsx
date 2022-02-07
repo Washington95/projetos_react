d3bf5fd788ab6ed836415010fadfcb362faaba4c
Washington95 <wst-washington@hotmail.com>
2022-02-07 20:50:34 -0300

    Estilizando inline

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,25 +1,14 @@
-import { useState } from "react";
-
 
 const App = () =>{
-  const [show, setshow] = useState(false);
 
-  const mostrar = () =>{
-    setshow( !show );
-  }
-  
-  
   return(
     <div>
-      <button onClick={mostrar}>{show ? 'Ver Menos' : 'Ver Mais'}</button>
+      <button style={{
+        backgroundColor: 'red',
+        color: 'white',
+        borderRadius:'5px'
+      }}>Clique aqui</button>
 
-      {show  &&
-        <div>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis inventore at reprehenderit magnam cupiditate rerum iste! Ex, minima iste optio, enim quis, asperiores nihil eius quas ipsa earum saepe unde?</p>
-        </div>
-      }
-      
-     
     </div>
       
   );
