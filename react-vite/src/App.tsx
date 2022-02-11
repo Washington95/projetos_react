7fd670fce4605e82524e010d9113544576bb59c8
Washington95 <wst-washington@hotmail.com>
2022-02-10 21:07:22 -0300

    Responsividade no TailwindCSS

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,9 +1,13 @@
 const App = () =>{
 
   return(
-    <div className="bg-slate-500 text-orange-900 p-3">
+    <div className="bg-slate-500 text-orange-900 p-3 font-bold flex flex-col text-center md:flex-row md:justify-between">
       Texto qualquer
+
+
+      <button className="bg-green-800 text-black p-2 rounded-xl mt-3 md:ml-3 md:mt-0 md:text-2xl " >Clique aqui</button>
     </div>
+    
 
       
   );
