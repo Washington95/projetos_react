f164032fa886e51770b1de8080cb331d591e83d7
Washington95 <wst-washington@hotmail.com>
2022-02-14 22:10:07 -0300

    Requisição GET simples 2

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import { Movie } from "./types/Movie";
 
 
 const App = () =>{
-  const [movies, setmovies] = useState([]);
+  const [movies, setmovies] = useState<Movie[]>([]);
  
   const carregar = () =>{
     fetch('https://api.b7web.com.br/cinema/')
@@ -17,10 +18,20 @@ const App = () =>{
 
   return(
     <div>
-        <button onClick={carregar}>Carregar Filmes</button>
+      <div className="grid justify-items-center">
+        <button className="block bg-blue-400 p-2 rounded " onClick={carregar}>Carregar Filmes</button> <br />
+      </div>
+        
 
-      Total de filmes : {movies.length}
-
+      <p className="text-center mb-2">Total de filmes : {movies.length} </p> 
+      <div className="grid grid-cols-4 gap-3 ml-3">
+        {movies.map((item, index) => (
+          <div key={index}>
+            <img src={item.avatar} className="w-28 block " alt="Capa" />
+            {item.titulo}
+          </div>
+        ))}
+      </div>
     </div>
       
   );
