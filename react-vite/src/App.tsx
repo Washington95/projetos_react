1ba1ad8ae627edeb8b59a02d50170823b0f8c86d
Washington95 <wst-washington@hotmail.com>
2022-02-14 21:24:02 -0300

    Requisição GET simples 1

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,29 +1,25 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { useState } from "react";
 
 
 const App = () =>{
-  const [name, set] = useState('');
-  const [sname, sets] = useState ('');
-  const [namec, setc] = useState ('');
-
-  useEffect(()=>{
-    setc(`${name} ${sname}`)
-  }, [name, sname])
-
-  const mname = (e: ChangeEvent<HTMLInputElement>) =>{
-    set(e.target.value)
-
-  } 
-
-  const msname = (e: ChangeEvent<HTMLInputElement>) =>{
-    sets(e.target.value)
+  const [movies, setmovies] = useState([]);
+ 
+  const carregar = () =>{
+    fetch('https://api.b7web.com.br/cinema/')
+      .then((response) =>{
+        return response.json();
+      })
+      .then((json)=>{
+        setmovies(json);
+      });
+      
   }
 
   return(
     <div>
-      <input type="text" placeholder="Digite seu Nome" value={name} onChange={mname} /> <br />
-      <input type="text" placeholder="Digite seu Sobrenome" value={sname} onChange={msname} />
-      <p>Nome Completo: {namec}</p>
+        <button onClick={carregar}>Carregar Filmes</button>
+
+      Total de filmes : {movies.length}
 
     </div>
       
