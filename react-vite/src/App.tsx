c41be4f7f1b9a899cbd2859492368328690dad20
Washington95 <wst-washington@hotmail.com>
2022-02-15 19:42:40 -0300

    Lidando com Loading

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -4,6 +4,7 @@ import { Movie } from "./types/Movie";
 
 const App = () =>{
   const [movies, setmovies] = useState<Movie[]>([]);
+  const [loading, setloading] = useState(false)
  
   // const carregar = () =>{
   //   fetch('https://api.b7web.com.br/cinema/')
@@ -17,8 +18,10 @@ const App = () =>{
   // }
 
   const carregar =  async () =>{
+    setloading(true);
     let response = await  fetch('https://api.b7web.com.br/cinema/');
     let json  = await response.json();
+    setloading(false);
     setmovies(json);
       
   }
@@ -26,20 +29,29 @@ const App = () =>{
 
   return(
     <div>
-      <div className="grid justify-items-center">
-        <button className="block bg-blue-400 p-2 rounded " onClick={carregar}>Carregar Filmes</button> <br />
-      </div>
-        
+      {loading &&
+        <div>
+          Carregando...
+        </div>
+      }
+      {!loading &&
+        <>
+        <div className="grid justify-items-center">
+          <button className="block bg-blue-400 p-2 rounded " onClick={carregar}>Carregar Filmes</button> <br />
+        </div>
+          
 
-      <p className="text-center mb-2">Total de filmes : {movies.length} </p> 
-      <div className="grid grid-cols-4 gap-3 ml-3">
-        {movies.map((item, index) => (
-          <div key={index}>
-            <img src={item.avatar} className="w-28 block " alt="Capa" />
-            {item.titulo}
-          </div>
-        ))}
-      </div>
+        <p className="text-center mb-2">Total de filmes : {movies.length} </p> 
+        <div className="grid grid-cols-4 gap-3 ml-3">
+          {movies.map((item, index) => (
+            <div key={index}>
+              <img src={item.avatar} className="w-28 block " alt="Capa" />
+              {item.titulo}
+            </div>
+          ))}
+        </div>
+        </>
+      }
     </div>
       
   );
