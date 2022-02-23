5a3223af634556acbf57600f25ee8c267c6ca3ca
Washington95 <wst-washington@hotmail.com>
2022-02-23 19:45:35 -0300

    Criando rotas

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,7 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+
 const App = () => {
   return (
-    <div>
-      ...
+    <div className="p-4">
+      <header>
+        <h1>Titulo da Página</h1>
+      </header>
+      <hr />
+      <div className="py-4">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/sobre" element={<About/>} />
+        </Routes>
+
+
+      </div>
+      <hr />
+      <footer>
+        Todos direitos reservados
+      </footer>
     </div>
   )
 }
