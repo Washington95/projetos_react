b536f0ff555fbb26ca734303e6c67664d137e3a2
Washington95 <wst-washington@hotmail.com>
2022-02-25 18:38:37 -0300

    Parâmetros de query

--- react-vite/src/pages/About.tsx
+++ react-vite/src/pages/About.tsx
@@ -1,11 +1,23 @@
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
 
 export const About = () => {
+    const [search, newsearch] = useSearchParams()
+
+    const org = (order: 'asc' | 'desc')  => {
+        search.set('order', order);
+        newsearch(search)
+    }
     return(
         <div>
+            Filtro: {search.get('filter')} <br />
+            Ordem: {search.get('order')}
+            <hr />
+            <button onClick={()=>org('asc')}>Asc</button>
+            <button onClick={()=>org('desc')}>Desc</button>
+            <hr />
             Página Sobre: 
             <ul>
-                
+                <li><Link to="/sobre/w"> W </Link></li>
                 <hr />
             </ul>
             
