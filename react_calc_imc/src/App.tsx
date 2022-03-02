1dc871316e3d7779daf3ac8da4a9f82c92186019
Washington95 <wst-washington@hotmail.com>
2022-03-01 21:12:24 -0300

    criando parte esquerda do site

--- react_calc_imc/src/App.tsx
+++ react_calc_imc/src/App.tsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
 import styles from './App.module.css';
 import poweredImage from './assets/powered.png';
 
 const App = () =>{
+  const [alt, newalt] = useState<number>(0)
+  const [pes, newpes] = useState<number>(0)
+
+  const calcular = () => {
+    if(alt && pes){
+      
+
+    }else{
+      alert("preencha todos os campos")
+    }
+  }
+
   return(
     <div className={styles.main}>
       <header>
@@ -11,7 +24,23 @@ const App = () =>{
       </header>
       <div className={styles.container}>
         <div className={styles.leftS}>
-          ...
+          <h1>Calcule o seu IMC.</h1>
+          <p>IMC é a sigla para Indice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
+
+          <input 
+            type="number"
+            placeholder= "Digite a sua altura Ex: 1.5 (em métros) "
+            value={alt > 0 ? alt : ''}
+            onChange={e => newalt(parseFloat(e.target.value))}
+          />
+          <input 
+            type="number"
+            placeholder='Digite seu peso Ex:75 (em kg)'
+            value={pes > 0 ? pes : ''}
+            onChange={e => newpes(parseFloat(e.target.value))}
+          />
+          <button onClick={calcular}>Calcluar</button>
+
         </div>
         <div className={styles.rightS}>
           ...
